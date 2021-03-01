8f1d646c3f985880c59f857b1bd828716e96948c
IVAN KULIASHOV <42475953+Krivetka@users.noreply.github.com>
2021-03-01 20:24:06 +0300

    feat: add min max

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+     if(array != null && array.length!=0){
+         minim=array[0];
+         for(i=0; i<array.length;i++){
+             if(array[i]<minim) minim=array[i];
+         }
+         return minim;
+     }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array != null && array.length!=0){
+        maxim=array[0];
+        for(i=0; i<array.length;i++){
+            if(array[i]>maxim) maxim=array[i];
+        }
+        return maxim;
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
+       if(array != null && array.length!=0){
+         sum=0;
+        for(i=0;i<array.length;i++){
+            sum+=array[i];
+        }
+        return (sum/array.length);
+     }
   return 0;
 }
